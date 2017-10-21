99f01c4a11b13f53b641415cdb7b7970f15c4c32
tiayo <656861622@qq.com>
2017-10-21 19:57:52 +0800

    修改

--- resources/views/manage/index/index.blade.php
+++ resources/views/manage/index/index.blade.php
@@ -7,17 +7,13 @@
 @endsection
 
 @section('body')
-    <div class="col-md-12 text-center">
+    <div class="col-md-12 text-center" style="margin-top: 18%">
         <h1>@if(can('admin'))
                 管理员后台
             @elseif(can('business'))
                 商户后台
                 @endif
         </h1>
-        <h1></h1>
-        <h4>欢迎您: {{ Auth::user()->name }}</h4>
-        <h4>服务器时间: {{ date('Y-m-d H:i:s') }}</h4>
-        <h4>登录ip:{{ Request::getClientIp() }}</h4>
     </div>
 @endsection
 
